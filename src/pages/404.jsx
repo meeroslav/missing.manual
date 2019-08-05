cd558d91ef31589ca061172389d4de8de4b7816b
Miroslav Jonas <missing.manual@gmail.com>
2019-08-05 23:24:43 +0200

    feat: add hero pictures to 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,9 +10,9 @@ export default class NotFoundPage extends Component {
     const siteTitle = data.site.siteMetadata.title;
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location} title={siteTitle} cover={data.cover}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
+        <h1>These are not the droids you are looking for</h1>
         <p>You just hit a route that doesn't exist... the sadness.</p>
       </Layout>
     )
@@ -24,6 +24,13 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    cover: file(absolutePath: { regex: "/pages\/404.jpg/" }) {
+      childImageSharp {
+        sizes(maxWidth: 2000, maxHeight: 600) {
+          ...GatsbyImageSharpSizes
+        }
       }
     }
   }
