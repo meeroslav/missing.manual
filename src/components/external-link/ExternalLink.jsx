0d5b4eada8693a37b64f293e9d8f4cf9fd132c95
Miroslav Jonas <missing.manual@gmail.com>
2019-08-20 16:20:04 +0200

    feat: do not open mailto in the new tab

--- src/components/external-link/ExternalLink.jsx
+++ src/components/external-link/ExternalLink.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export default class ExternalLink extends Component {
   render() {
     const { to, children, className } = this.props;
+    const target = to.startsWith('mailto:') ? '_self' : '_blank';
 
     return (
-      <a className={className} href={to} target="_blank" rel="noopener noreferrer">
+      <a className={className} href={to} target={target} rel="noopener noreferrer">
         {children}
       </a>
     );
