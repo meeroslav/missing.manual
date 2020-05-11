14b58d6f931230b6d8375e2fe6dea42a9e788c83
Miroslav Jonas <missing.manual@gmail.com>
2020-05-11 23:51:35 +0200

    feat: cleanup

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -10,7 +10,6 @@ const Layout = ({ children, ...props }) => {
   const title = props.title || props.pathContext.frontmatter.title;
   const description = props.description;
   const hero = props.hero || (props.data && props.data.cover.childImageSharp.sizes);
-  console.log(props);
   return (
     <div className={style.layout}>
       <SEO title={title} description={description} />
