f194d434bfc58f0bba74abd98752aede7b3213dc
Miroslav Jonas <missing.manual@gmail.com>
2020-09-16 13:53:30 +0200

    feat(tags): show tags on all pages

--- src/components/mini-post/MiniPost.jsx
+++ src/components/mini-post/MiniPost.jsx
@@ -6,6 +6,10 @@ import style from './mini-post.module.scss';
 const MiniPost = props => {
   const title = props.frontmatter.title || props.fields.slug;
   const excerpt = props.frontmatter.description || props.excerpt;
+  const tags = props.frontmatter.tags
+    ? props.frontmatter.tags.split(',').map(tag => tag.trim())
+    : null;
+
   return (
     <div key={props.fields.slug} className={style.miniPost}>
       <Image className={style.miniPostImage} fluid={props.frontmatter.cover.childImageSharp.fluid} />
@@ -17,6 +21,12 @@ const MiniPost = props => {
         </h3>
         <small className={style.miniPostDate}>
           {props.frontmatter.date}
+          {tags && (` ・ `)}
+          {tags && (<span className="tags">
+            { tags.map(t => (
+              <span className="tag">{t}</span>
+            ))}
+          </span>)}
         </small>
         <p
           dangerouslySetInnerHTML={{
