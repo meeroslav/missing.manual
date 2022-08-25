3d3bf57db709dc938f5aa7f945bb994d44812eee
Miroslav Jonas <missing.manual@gmail.com>
2022-08-25 11:16:58 +0200

    fix: make robots permissive

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -30,7 +30,8 @@ const config: GatsbyConfig = {
     },
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap', {
+    'gatsby-plugin-sitemap',
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         icon: 'src/images/icon.png'
